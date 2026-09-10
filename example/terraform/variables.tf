956c61eabe80aa73ffa62001b278a1344b9a8b14
-- Mustafa -- <37668530+gulercan@users.noreply.github.com>
2026-09-10 21:31:19 +0100

    BLD-357 terraform:SQL2022DEV-2026Q4V2

--- example/terraform/variables.tf
+++ example/terraform/variables.tf
@@ -12,7 +12,7 @@ variable "goog_cm_deployment_name" {
 variable "source_image" {
   description = "The image name for the disk for the VM instance."
   type        = string
-  default     = "projects/mpi-gclouds-public/global/images/gclouds-win2022-sql2022-dev-1779804685"
+  default     = "projects/mpi-gclouds-public/global/images/gclouds-win2022-sql2022-dev-1788818451"
 }
 
 variable "zone" {
